9e8e61997244489e52bd5ba049f0f267e6b008af
EslamKamel89 <elsamkamelforex@gmail.com>
2025-06-07 11:40:28 +0300

    Implement product update functionality with validation and error handling

--- server/api/admin/products/[productId].put.ts
+++ server/api/admin/products/[productId].put.ts
@@ -1,3 +1,24 @@
+import prisma from "~/utils/db";
+import { productSchema } from "~/utils/validation";
+
 export default defineEventHandler(async (event) => {
-  return { message: "hello world" };
+  const session = await requireUserSession(event);
+  const body = await readValidatedBody(event, (data) =>
+    productSchema.parse(data),
+  );
+  const productId = Number(getRouterParam(event, "productId"));
+  let product = await prisma.product.findUnique({
+    where: { id: productId },
+  });
+  if (!product) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Product not found in the database",
+    });
+  }
+  product = await prisma.product.update({
+    where: { id: productId },
+    data: { ...body, images: undefined },
+  });
+  return product;
 });
