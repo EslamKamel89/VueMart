895e219b981da376d3be9029d0cb4132ec613a82
EslamKamel89 <elsamkamelforex@gmail.com>
2025-06-07 11:54:43 +0300

    Implement product deletion functionality with session validation and confirmation message

--- server/api/admin/products/[productId].delete.ts
+++ server/api/admin/products/[productId].delete.ts
@@ -1,3 +1,11 @@
+import prisma from "~/utils/db";
+
 export default defineEventHandler(async (event) => {
-  return { message: "hello world" };
+  const session = await requireUserSession(event);
+  const productId = Number(getRouterParam(event, "productId"));
+  const product = await prisma.product.delete({
+    where: { id: productId },
+  });
+
+  return { message: "Product Deleted" };
 });
